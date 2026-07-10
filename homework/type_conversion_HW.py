8679041282abb95187e814222598d158996a6f98
jtugwuegbu-hub <jtugwuegbu@gmail.com>
2026-07-10 19:18:38 -0400

    Feat/type conversion Hw This is the homework, for type conversions

---
+++ homework/type_conversion_HW.py
@@ -0,0 +1,8 @@
+#The user inputs legnth in centimeters
+Length_cm = int(input("What is the length in cm? "))
+
+#Converting centimeters into meters
+Length_m = Length_cm/100
+
+#printing the legnth in meters
+print("The length in meters is:", Length_m,"m",sep="")
