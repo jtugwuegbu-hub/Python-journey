2498efd20fd8d06bd7bf64c56b323c597938f435
jtugwuegbu-hub <jtugwuegbu@gmail.com>
2026-07-20 20:30:01 -0400

    changes

---
+++ Personal work/edx.py
@@ -0,0 +1,7 @@
+count=0  #variable initialization                #i= special loop variable
+
+while count <= 10:  # for i in range(n)
+    print(count)
+    count+=1  # count=count+1  if we forget to write this we will accidently create an 
+     #infinite loop
+print("Done")
