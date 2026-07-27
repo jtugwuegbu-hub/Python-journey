d350721e1dee3d5a6972b3fff693e992c8371567
jtugwuegbu-hub <jtugwuegbu@gmail.com>
2026-07-27 11:38:51 -0400

    simple model

--- Sideline Seats/test.py
+++ Sideline Seats/test.py
@@ -1,26 +1,76 @@
-chair_id={
-    1234:{"name":"chair 1", "available":True},
-    2345:{"name":"chair 2", "available":True},
-    3456:{"name":"chair 3", "available":True},
-    4567:{"name":"chair 4", "available":True},
-    5678:{"name":"chair 5", "available":True},
-    6789:{"name":"chair 6", "available":True},
-    7890:{"name":"chair 8", "available":True},
-    8901:{"name":"chair 7", "available":True}
-}
+available_chairs = [1,2,3,4,5,6,7,8,9,10]
+rented_chairs = []
 
-#start of user interaction with machine
-rent_return= input("Please choose between rent and return.\n")
-
-if rent_return=="rent":
-    while True:
-        rented_chairs=int(input("you selected rent, please select the amount of chairs\n"))
-        if rented_chairs < 0:
-            continue
-        #elif rented_chair > then the amount available
-            #print ("THere are not enough chairs, x amount of chairs are available right now")
-            #continue
+while True:
+    user_menu=int(input(
+    "Please select the number from the following options?\n"
+    "1.Rent chair\n"
+    "2.Return chair\n"
+    "3.Check inventory\n"
+    "4.exit\n"))
+    
+    #Renting Chair
+    if user_menu == 1:
+        while True: #loop is useless aslong as it goes back to the other loop
+            print(f"The amount of available chairs:{len(available_chairs)} chairs")
+            num_chairs_rent=int(input("How many chairs would you like to rent?\n"))
+            if num_chairs_rent == 0:
+                print("Select a valid number of chairs")
+                break
+            elif num_chairs_rent > len(available_chairs):
+                print(f"There are not enough chairs to rent {num_chairs_rent}, only {len(available_chairs)} available")
+                break
+            elif num_chairs_rent <= len(available_chairs):
+                print(f"you are about to rent out {num_chairs_rent} chairs")
+                for chair in range(num_chairs_rent):
+                    chair=available_chairs.pop(0)
+                    rented_chairs.append(chair)
+                    print(f"You rented chairs: {chair}") #eventually print the code from dictionary and it will be needed to retun the chair
+                break
+            else:
+                print("Invalid statement")
+                break
+    elif user_menu == 2:
+        while True: #right now this loop is useless, but need a way for it to break
+            print("You selected return chairs")
+            num_chairs_return=int(input("How many chairs would you like to return\n"))
+            if num_chairs_return == 0:
+                print("You must return 1 chair")
+                break
+            elif num_chairs_return > len(rented_chairs):
+                print("There are not that many chairs rented out")
+                break
+            elif num_chairs_return<= len(rented_chairs):
+                for chair in range(num_chairs_return):
+                    chair=rented_chairs.pop(0)
+                    available_chairs.append(chair)
+                    print(f"You returned chair: {chair}")
+                break
+            else:
+                print("Invalid Statement")
+                break
+    #Check inventory
+    elif user_menu == 3:
+        if len(available_chairs) == 0:
+            print("There are no chairs available")
         else:
-            break
-    print(f"You selected {rented_chairs} chairs to rent")
+            print("Chair inventory:", end = "")
+            for chair in available_chairs:
+                print(chair, end = ", ")
+        if len(rented_chairs)== 0:
+            print()
+            print("No chairs rented out")
+        else:    
+            print()
+            print("Chairs rented out:", end = "")
+            for chair in rented_chairs:
+                print(chair, end = ", ")
+            print()
+        
+
+
+
+
+
+
 
