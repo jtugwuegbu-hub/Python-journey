824888128c3fde70da5797e5a958cbf51a103a4a
jtugwuegbu-hub <jtugwuegbu@gmail.com>
2026-07-28 15:25:55 -0400

    class

--- homework/list.py
+++ homework/list.py
@@ -1,9 +1,9 @@
+# list1=[1,2,3,4,5,-1,-3,4,2]
+# smallest=list1[0]
+# for number in list1:
+#     if smallest>number:
+#         smallest=number
+# print(smallest)
 
 
-list1=[32,323,3121,33,44,2,67,45,345,553,45,35,4]
-large=list1[0]
-for number in list1:
-    if number > large:
-        large = number
-print(f" the largest number is {large}")
 
