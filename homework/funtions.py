673f920351f9d9ccd9b1581010c86458d8473fc2
jtugwuegbu-hub <jtugwuegbu@gmail.com>
2026-08-03 14:47:52 -0400

    class

--- homework/funtions.py
+++ homework/funtions.py
@@ -1,24 +1,24 @@
-# def math():
+# def divide():
 #     number=float(input("Choose number: "))
 #     number= (number+10)/2
 #     return number
 
-# answer=math()
+# answer=divide()
 # print(answer)
 
-# x=float(input("number?"))
-# y=float(input("number?"))
-# z=float(input("number?"))
+# x=float(input("number? "))
+# y=float(input("number? "))
+# z=float(input("number? "))
 
 # def average(x,y,z):
-#     sum=(x+y+z) #sum of numbers
-#     average=sum/3 #average of numbers
+#     sum_num=(x+y+z) #sum of numbers
+#     average=sum_num/3 #average of numbers
 #     print(f"The sum is {sum}\nThe average is {average}")
-
+    
 # average(x,y,z)
 
-def equation(x,y,z):
+def equation(x=3,y=3,z=3):
     result=(x/y)*z
     print(result)
-
+equation()
 equation(20,z=3,y=10)
---
+++ homework/return_statement.py
@@ -0,0 +1,13 @@
+
+def sum1(x,y,z):
+    sum_num=x+y+z
+    return sum_num
+
+
+def average(x,y,z):
+    sum_num=sum1(x,y,z)
+    average=sum_num/3
+    print(average)
+
+average(5,10,6)
+
